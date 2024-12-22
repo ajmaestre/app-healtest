e24b60bae966b5f664973ff4748ea0506fa2d1c1
ajmaestre <ajmaestretorres@gmail.com>
2024-12-22 17:47:30 -0500

    updating app.* files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule, 
+    RouterModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'app-healtest';
+
+  title = 'HealTest';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { ExtraOptions, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { PatientComponent } from './patient/patient.component';
+import { HomeComponent } from './home/home.component';
+import { adminGuard } from './auth/admin.guard';
+import { doctorGuard } from './auth/doctor.guard';
+import { patientGuard } from './auth/patient.guard';
+import { TestingPanelPatientComponent } from './testing-panel-patient/testing-panel-patient.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'admin', component: AdminComponent, canActivate: [adminGuard] },
+    { path: 'doctor', component: DoctorComponent, canActivate: [doctorGuard] },
+    { path: 'patient', component: PatientComponent, canActivate: [patientGuard] },
+    { path: 'testing', component: TestingPanelPatientComponent, canActivate: [patientGuard] },
+    { path: '**', redirectTo: 'home' }
+];
+
+
+export const routesOptions: ExtraOptions = {
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'enabled',
+    scrollOffset: [0, 64],
+}
