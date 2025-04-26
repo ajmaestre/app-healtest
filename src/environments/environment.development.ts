19bad05c112984e6319767361d79d169422c8db0
ajmaestre <ajmaestretorres@gmail.com>
2025-04-26 09:52:02 -0500

    Cambiando la ruta de la api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    // BASE_URL: 'https://api-healtest.onrender.com'
-    BASE_URL: 'http://localhost:3000'
+    BASE_URL: 'https://api-healtest.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    // BASE_URL: 'https://api-healtest.onrender.com',
-    BASE_URL: 'http://localhost:3000'
+    BASE_URL: 'https://api-healtest.onrender.com',
   };
